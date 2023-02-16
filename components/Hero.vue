be03d727baa03c5759cfab7fa83d2180fbb458b5
souravthakur1992 <souravthakur1992@gmail.com>
2023-02-16 08:51:57 +0100

    Update Hero.vue

--- components/Hero.vue
+++ components/Hero.vue
@@ -11,10 +11,10 @@
               <p class="pt-4 font-ibm text-base font-normal text-white">
                 100% prepaid data packs with no hidden fees. <span class="underline"> Learn more</span>
               </p>
-              <div class="mt-10 flex justify-between rounded-md bg-white pl-8 pr-2 shadow md:mt-10 lg:max-w-[540px]">
+              <div class="mt-10 flex justify-between rounded-lg bg-white pl-8 pr-1.5 shadow md:mt-10 lg:max-w-[540px]">
                 <div class="flex items-center gap-2.5">
                   <div>
-                    <input type="search" placeholder="Label" class="h-14 border-0 font-ibm font-normal text-black outline-0" />
+                    <input type="search" placeholder="Label" class="h-12 border-0 font-ibm font-normal text-black outline-0" />
                   </div>
                 </div>
                 <div class="flex items-center">
@@ -38,12 +38,12 @@
         </div>
         <div class="filter-Card relative flex justify-center">
           <ul
-            class="filters realestate-filter mx-auto mt-7 flex w-full max-w-[350px] flex-row justify-evenly gap-5 overflow-auto whitespace-nowrap rounded-md bg-white px-4 py-2 font-medium shadow"
+            class="filters realestate-filter mx-auto mt-7 flex h-11 w-full max-w-[350px] flex-row justify-evenly gap-5 overflow-auto whitespace-nowrap rounded-md bg-white px-3 py-2 font-medium shadow"
           >
             <li class="filter" :class="{ active: activeTab === 'Local' }">
               <button
                 type="button"
-                class="btn rounded-none px-5 py-1 font-ibm text-sm font-medium capitalize text-tabGrey hover:rounded-[4px] hover:bg-black hover:text-white"
+                class="btn rounded-none px-6 py-1 font-ibm text-sm font-medium capitalize text-tabGrey hover:rounded-[4px] hover:bg-black hover:text-white"
                 @click="onTabChange('Local')"
               >
                 Local
@@ -52,7 +52,7 @@
             <li class="filter" :class="{ active: activeTab === 'Regional' }">
               <button
                 type="button"
-                class="btn rounded-none px-5 py-1 font-ibm text-sm font-medium capitalize text-tabGrey hover:rounded-[4px] hover:bg-black hover:text-white"
+                class="btn rounded-none px-6 py-0.5 font-ibm text-sm font-medium capitalize text-tabGrey hover:rounded-[4px] hover:bg-black hover:text-white"
                 @click="onTabChange('Regional')"
               >
                 Regional
@@ -61,7 +61,7 @@
             <li class="filter" :class="{ active: activeTab === 'Global' }">
               <button
                 type="button"
-                class="btn rounded-none px-5 py-1 font-ibm text-sm capitalize text-tabGrey hover:rounded-[4px] hover:bg-black hover:text-white"
+                class="btn rounded-none px-6 py-0.5 font-ibm text-sm capitalize text-tabGrey hover:rounded-[4px] hover:bg-black hover:text-white"
                 @click="onTabChange('Global')"
               >
                 Global
